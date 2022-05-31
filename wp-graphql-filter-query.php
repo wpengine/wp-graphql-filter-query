cb09895fe46f86b92db199378583b5675cc732a5
Shane Daly <shane.daly@wpengine.com>
2022-05-31 10:45:46 +0100

    chore: adding wp scaffold plugin assets (#1)

---
+++ wp-graphql-filter-query.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Graphql Filter Query
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-graphql-filter-query
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Graphql_Filter_Query
+ */
+
+// Your code starts here.
