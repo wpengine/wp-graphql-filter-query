afa79b13266e296a06774f60a5b7ca5a44ee38a6
Shane Daly <shane.daly@wpengine.com>
2022-06-01 16:26:37 +0100

    test: add test sample with wp-graphql installed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -31,6 +31,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/wp-graphql-filter-query.php';
+	require dirname( dirname( __FILE__ ) ) .'/vendor/wp-graphql/wp-graphql/wp-graphql.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
