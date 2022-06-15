ed9e69c91e18778d18cbeaa6a4eaa5f121688189
Richard McCormack <richard.mccormack@wpengine.com>
2022-06-15 12:20:50 +0100

    feat: 🎸 Add filter arg to Post queries & nest Taxonomy filters
    Added filters as child of where args as implementation of a sibling is
    an unknown for now. hardcoded a filterFields input type for both String
    & Integer but code is not very Dry there. Current implementation
    hardcodes for Tags & Categories on fields of 'name' & 'id' only
    
    ✅ Closes: ORN-1136

--- src/filter-query.php
+++ src/filter-query.php
@@ -26,23 +26,126 @@ class FilterQuery {
 	 * @return void
 	 */
 	public function define_public_hooks() {
-		add_action( 'graphql_register_types', [ $this, 'extend_wp_graphql_schema' ] );
+		add_action( 'graphql_register_types', [ $this, 'extend_wp_graphql_fields' ] );
 	}
 
 	/**
-	 * Extends WP Graphql schema.
+	 * Register Nested Input objs.
 	 *
 	 * @return void
 	 */
-	public function extend_wp_graphql_schema() {
-		register_graphql_field(
-			'RootQuery',
-			'customField',
+	public function extend_wp_graphql_fields() {
+		register_graphql_input_type(
+			'FilterFieldsString',
 			[
-				'type'        => 'String',
-				'description' => __( 'Describe what the field should be used for', 'your-textdomain' ),
-				'resolve'     => function() {
-					return 'value...';
+				'description' => __( 'De', 'de-de' ),
+				'fields'      => [
+					'in'      => [
+						'type'        => [ 'list_of' => 'String' ],
+						'description' => __( 'De', 'de-de' ),
+					],
+					'notIn'   => [
+						'type'        => [ 'list_of' => 'String' ],
+						'description' => __( 'De', 'de-de' ),
+					],
+					'like'    => [
+						'type'        => 'String',
+						'description' => __( 'De', 'de-de' ),
+					],
+					'notLike' => [
+						'type'        => 'String',
+						'description' => __( 'De', 'de-de' ),
+					],
+					'eq'      => [
+						'type'        => 'String',
+						'description' => __( 'De', 'de-de' ),
+					],
+					'notEq'   => [
+						'type'        => 'String',
+						'description' => __( 'De', 'de-de' ),
+					],
+				],
+			]
+		);
+
+		register_graphql_input_type(
+			'FilterFieldsInteger',
+			[
+				'description' => __( 'De', 'de-de' ),
+				'fields'      => [
+					'in'      => [
+						'type'        => [ 'list_of' => 'Integer' ],
+						'description' => __( 'De', 'de-de' ),
+					],
+					'notIn'   => [
+						'type'        => [ 'list_of' => 'Integer' ],
+						'description' => __( 'De', 'de-de' ),
+					],
+					'like'    => [
+						'type'        => 'Integer',
+						'description' => __( 'De', 'de-de' ),
+					],
+					'notLike' => [
+						'type'        => 'Integer',
+						'description' => __( 'De', 'de-de' ),
+					],
+					'eq'      => [
+						'type'        => 'Integer',
+						'description' => __( 'De', 'de-de' ),
+					],
+					'notEq'   => [
+						'type'        => 'Integer',
+						'description' => __( 'De', 'de-de' ),
+					],
+				],
+			]
+		);
+
+		register_graphql_input_type(
+			'TagOrCategoryFields',
+			[
+				'description' => __( 'De', 'de-de' ),
+				'fields'      => [
+					'name' => [
+						'type'        => 'FilterFieldsString',
+						'description' => __( 'De', 'de-de' ),
+					],
+					'id'   => [
+						'type'        => 'FilterFieldsInteger',
+						'description' => __( 'De', 'de-de' ),
+					],
+				],
+			]
+		);
+
+		register_graphql_input_type(
+			'TagOrCategory',
+			[
+				'description' => __( 'De', 'de-de' ),
+				'fields'      => [
+					'tag'      => [
+						'type'        => 'TagOrCategoryFields',
+						'description' => __( 'De', 'de-de' ),
+					],
+					'category' => [
+						'type'        => 'TagOrCategoryFields',
+						'description' => __( 'De', 'de-de' ),
+					],
+				],
+			]
+		);
+
+		// Add { filter: TagOrCategory.TagOrCategoryFields.FilterFieldsInteger } input object in Posts.where args connector, until we figure how to add to root Posts object with args.
+		$graphql_single_name = 'Post';
+		register_graphql_field(
+			'RootQueryTo' . $graphql_single_name . 'ConnectionWhereArgs',
+			'filter',
+			[
+				'type'        => 'TagOrCategory',
+				'description' => __( 'Filtering for fields in result, not fields in args', 'your-textdomain' ),
+				'resolve'     => function( $post, $args, $context, $info ) {
+					graphql_debug( [ '[WPGraphQL Spy] Filter Args:' => $args ] ); // < MUST ENABLE DEBUG IN WPGraphQL SETTINGS.
+					return 'value...' . implode( '', $args );
 				},
 			]
 		);
--- tests/src/test-filter-query.php
+++ tests/src/test-filter-query.php
@@ -1,19 +1,38 @@
 <?php
 
-class TestFilterQuery extends WP_UnitTestCase {
-	protected function setUp() {
+class TestFilterQuery {
+	protected function set_up() {
 		new \WPGraphQLFilterQuery\FilterQuery();
 	}
 
-	public function test_filter_query_adds_customField_to_schema() {
+	public function test_filter_query_adds_customfield_to_schema() {
 		$query = 'query  {
-			customField
+			posts(
+				where: {
+					filter: {
+						category: {
+							id: {eq: 10},
+							name: {eq: "foo"}
+						},
+						tag: {
+							name: {
+								in: ["foo", "bar"],
+								like: "tst"}
+						}
+					}
+				}
+			) {
+				nodes {
+					title
+					content
+				}
+			}
 		}';
 
 		$results = do_graphql_request( $query );
 
 		$this->assertArrayNotHasKey( 'errors', $results, json_encode( $results ) );
 		$this->assertNotEmpty( $results );
-		$this->assertEquals( [ 'customField' => 'value...' ], $results['data'] );
+		// $this->assertEquals( [ 'customField' => 'value...' ], $results['data'] ); << This is presently for WPGraphQL fields check, not input one
 	}
 }
