b11867c48de27dfb3f413d601a4e301ca3bc76e1
Richard McCormack <richard.mccormack@wpengine.com>
2022-06-16 11:41:59 +0100

    refactor: 💡 Extend & Fix Unit Test Errs for Test-Filter-Query

--- tests/src/test-filter-query.php
+++ tests/src/test-filter-query.php
@@ -1,37 +1,67 @@
 <?php
 
-class TestFilterQuery {
-	protected function set_up() {
+class TestFilterQuery extends WP_UnitTestCase {
+	protected function setUp() {
 		new \WPGraphQLFilterQuery\FilterQuery();
 	}
 
 	public function test_filter_query_adds_customfield_to_schema() {
-		$query = 'query  {
-			posts(
-				where: {
-					filter: {
-						category: {
-							id: {eq: 10},
-							name: {eq: "foo"}
-						},
-						tag: {
-							name: {
-								in: ["foo", "bar"],
-								like: "tst"}
+		$query_and_results = array(
+			'query1'        => 'query  {
+				posts(
+					where: {
+						filter: {
+							category: {
+								id: {eq: 10},
+								name: {eq: "foo"}
+							},
+							tag: {
+								name: {
+									in: ["foo", "bar"],
+									like: "tst"}
+							}
 						}
 					}
+				) {
+					nodes {
+						title
+						content
+					}
 				}
-			) {
-				nodes {
-					title
-					content
-				}
-			}
-		}';
+			}',
+			'result1_IsNot' => 'errors',
+			'query2'        =>
+				'query  {
+					posts(
+						where: {
+							filter: {
+								category: {
+									id: {eq: "10"},
+									name: {eq: "foo"}
+								},
+								tag: {
+									name: {
+										in: ["foo", "bar"],
+										like: "tst"}
+								}
+							}
+						}
+					) {
+						nodes {
+							title
+							content
+						}
+					}
+				}',
+			'result2_IsNot' => 'data',
+		);
 
-		$results = do_graphql_request( $query );
+		$results1 = do_graphql_request( $query_and_results['query1'] );
+		$this->assertArrayNotHasKey( $query_and_results['result1_IsNot'], $results1, json_encode( $results1 ) );
+		$this->assertNotEmpty( $results1 );
 
-		$this->assertArrayNotHasKey( 'errors', $results, json_encode( $results ) );
-		$this->assertNotEmpty( $results );
+		$results2 = do_graphql_request( $query_and_results['query2'] );
+		$this->assertArrayNotHasKey( $query_and_results['result2_IsNot'], $results2, json_encode( $results2 ) );
+		$this->assertNotEmpty( $results2 );
 	}
 }
