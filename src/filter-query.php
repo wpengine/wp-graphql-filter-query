bed54b6a75009c0fe642fd238482fe0523771f5b
Richard McCormack <85675127+richard-wpengine@users.noreply.github.com>
2022-06-17 15:03:45 +0100

    feat: 🎸 Add filter arg to Post queries & nest Taxonomy filters (#6)
    * feat: 🎸 Add filter arg to Post queries & nest Taxonomy filters
    
    Added filters as child of where args as implementation of a sibling is
    an unknown for now. hardcoded a filterFields input type for both String
    & Integer but code is not very Dry there. Current implementation
    hardcodes for Tags & Categories on fields of 'name' & 'id' only
    
    ✅ Closes: ORN-1136
    
    * refactor: 💡 Updated WPGraphQL fields descriptions & tidied code
    
    * refactor: 💡 Extend & Fix Unit Test Errs for Test-Filter-Query
    
    * refactor: 💡 Extended Filters to Users & Pages, & Unit Test This
    
    * refactor: 💡 Linter tidying
    
    * refactor: 💡 Shane Suggestions, incl. More Explicit Unit Test Fn
    
    * refactor: 💡 Replaced Filter on User to Page, as User has no Tax
    
    Updated associated tests & added CPT.where.filter support
    
    * refactor: 💡 Updated filter tests to a Provider pattern
    
    Also moved form kvp test pattern to jagged array pattern in content
    engine
    
    * refactor: 💡 PR comments
    
    * refactor: 💡 Removed truthy unit args, as per PR suggestions
    
    * chore: move tests to data provider pattern (#7)
    
    * chore: move tests to data provider pattern
    
    * chore: update test names
    
    * chore: update data_provider return type
    
    * chore: add test with cpt example (#8)
    
    Co-authored-by: Shane Daly <shane.daly@wpengine.com>

--- src/filter-query.php
+++ src/filter-query.php
@@ -26,25 +26,142 @@ class FilterQuery {
 	 * @return void
 	 */
 	public function define_public_hooks() {
-		add_action( 'graphql_register_types', [ $this, 'extend_wp_graphql_schema' ] );
+		add_action( 'graphql_register_types', [ $this, 'extend_wp_graphql_fields' ] );
 	}
 
 	/**
-	 * Extends WP Graphql schema.
+	 * Register Nested Input objs.
 	 *
 	 * @return void
 	 */
-	public function extend_wp_graphql_schema() {
-		register_graphql_field(
-			'RootQuery',
-			'customField',
+	public function extend_wp_graphql_fields() {
+		register_graphql_input_type(
+			'FilterFieldsString',
 			[
-				'type'        => 'String',
-				'description' => __( 'Describe what the field should be used for', 'your-textdomain' ),
-				'resolve'     => function() {
-					return 'value...';
-				},
+				'description' => __( 'String Field Match Arguments', 'wp-graphql-filter-query' ),
+				'fields'      => [
+					'in'      => [
+						'type'        => [ 'list_of' => 'String' ],
+						'description' => __( 'For This To Be Truthy, At Least One Item Of The String Array Arg Passed Here Must Be Contained Within The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'notIn'   => [
+						'type'        => [ 'list_of' => 'String' ],
+						'description' => __( 'For This To Be Truthy, Not One Item Of The String Array Arg Passed Here Can Be Contained Within The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'like'    => [
+						'type'        => 'String',
+						'description' => __( 'For This To Be Truthy, The Arg Passed Here Must Relate To The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'notLike' => [
+						'type'        => 'String',
+						'description' => __( 'For This To Be Truthy, The Arg Passed Here Must Not Relate To The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'eq'      => [
+						'type'        => 'String',
+						'description' => __( 'For This To Be Truthy, The Arg Passed Here Must Be An Exact Match To The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'notEq'   => [
+						'type'        => 'String',
+						'description' => __( 'For This To Be Truthy, The Arg Passed Here Must Not Match To The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+				],
 			]
 		);
+
+		register_graphql_input_type(
+			'FilterFieldsInteger',
+			[
+				'description' => __( 'Integer Field Match Arguments', 'wp-graphql-filter-query' ),
+				'fields'      => [
+					'in'      => [
+						'type'        => [ 'list_of' => 'Integer' ],
+						'description' => __( 'For This To Be Truthy, At Least One Item Of The String Array Arg Passed Here Must Be Contained Within The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'notIn'   => [
+						'type'        => [ 'list_of' => 'Integer' ],
+						'description' => __( 'For This To Be Truthy, Not One Item Of The String Array Arg Passed Here Can Be Contained Within The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'like'    => [
+						'type'        => 'Integer',
+						'description' => __( 'For This To Be Truthy, The Arg Passed Here Must Relate To The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'notLike' => [
+						'type'        => 'Integer',
+						'description' => __( 'For This To Be Truthy, The Arg Passed Here Must Not Relate To The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'eq'      => [
+						'type'        => 'Integer',
+						'description' => __( 'For This To Be Truthy, The Arg Passed Here Must Be An Exact Match To The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+					'notEq'   => [
+						'type'        => 'Integer',
+						'description' => __( 'For This To Be Truthy, The Arg Passed Here Must Not Match To The Calling Taxonomy Field, By Way Of Predefined Aggregates', 'wp-graphql-filter-query' ),
+					],
+				],
+			]
+		);
+
+		register_graphql_input_type(
+			'TaxonomyFilterFields',
+			[
+				'description' => __( 'Taxonomy fields For Filtering', 'wp-graphql-filter-query' ),
+				'fields'      => [
+					'name' => [
+						'type'        => 'FilterFieldsString',
+						'description' => __( 'ID field For Filtering, Of Type String', 'wp-graphql-filter-query' ),
+					],
+					'id'   => [
+						'type'        => 'FilterFieldsInteger',
+						'description' => __( 'ID field For Filtering, Of Type Integer', 'wp-graphql-filter-query' ),
+					],
+				],
+			]
+		);
+
+		register_graphql_input_type(
+			'TaxonomyFilter',
+			[
+				'description' => __( 'Taxonomies Where Filtering Supported', 'wp-graphql-filter-query' ),
+				'fields'      => [
+					'tag'      => [
+						'type'        => 'TaxonomyFilterFields',
+						'description' => __( 'Tags Object Fields Allowable For Filtering', 'wp-graphql-filter-query' ),
+					],
+					'category' => [
+						'type'        => 'TaxonomyFilterFields',
+						'description' => __( 'Category Object Fields Allowable For Filtering', 'wp-graphql-filter-query' ),
+					],
+				],
+			]
+		);
+
+		// Add { filter: TagOrCategory.TagOrCategoryFields.FilterFieldsInteger } input object in Posts.where args connector, until we figure how to add to root Posts object with args.
+		$default_supported_wp_types = array( 'Post', 'Page' );
+		$cpt_type_names             = get_post_types(
+			array(
+				'public'   => true,
+				'_builtin' => false,
+			),
+			'names'
+		);
+
+		foreach ( $cpt_type_names as $name ) {
+			$cpt_type_names[ $name ] = ucwords( $name );
+		}
+
+		$taxonomy_filter_supported_types = array_merge( $default_supported_wp_types, $cpt_type_names );
+
+		foreach ( $taxonomy_filter_supported_types as &$type ) {
+			$graphql_single_name = $type;
+			register_graphql_field(
+				'RootQueryTo' . $graphql_single_name . 'ConnectionWhereArgs',
+				'filter',
+				[
+					'type'        => 'TaxonomyFilter',
+					'description' => __( 'Filtering Queried Results By Taxonomy Objects', 'wp-graphql-filter-query' ),
+				]
+			);
+		}
+		unset( $type );
 	}
 }
